125c4c0e4f678273493d02f8c122aec62c993da5
Julian Sotelo <juansoos@protonmail.com>
2023-09-20 14:55:40 -0500

    feat(Locations): add support for infinite scroll (#12)

--- src/screens/locations/locations.screen.tsx
+++ src/screens/locations/locations.screen.tsx
@@ -1,25 +1,53 @@
 import React, {FC, useState, useEffect} from 'react';
 import {FlatList} from 'react-native';
+import uuid from 'react-native-uuid';
 import {Layout} from '../../components/layout/layout.component';
 import {Location} from '../../api/model/location.model';
 import {GetAllLocations} from '../../api/repository/locations';
-import {Loader} from '../../components';
+import {Footer, Loader} from '../../components';
 import {LocationItem} from './components';
 
 export const Locations: FC = () => {
   const [isLoading, setIsLoading] = useState(true);
-  const [locations, setLocations] = useState<Array<Location>>([]);
+  const [isMoreLoading, setIsMoreLoading] = useState(false);
+  const [hasMoreLocations, setHasMoreLocations] = useState(true);
 
-  const getAllLocations = async () => {
+  const [locations, setLocations] = useState<Array<Location>>([]);
+  const [page, setPage] = useState(1);
+
+  const getInitialLocations = async () => {
     const response = await GetAllLocations();
 
     setIsLoading(false);
     setLocations(response.results);
+    setHasMoreLocations(response.info?.next != null);
+  };
+
+  const fetchMoreLocations = async () => {
+    if (hasMoreLocations) {
+      const nextPage = page + 1;
+      setIsMoreLoading(true);
+
+      const response = await GetAllLocations(nextPage);
+
+      setIsMoreLoading(false);
+      setLocations(prev => [...prev, ...response.results]);
+      setPage(nextPage);
+      setHasMoreLocations(response.info?.next != null);
+    }
   };
 
   useEffect(() => {
-    getAllLocations();
+    getInitialLocations();
   }, []);
+
+  const renderFooter = () => (
+    <Footer
+      isMoreLoading={isMoreLoading}
+      hasMoreCharacters={hasMoreLocations}
+      text={' No more episodes at the moment'}
+    />
+  );
 
   return (
     <Layout>
@@ -28,10 +56,13 @@ export const Locations: FC = () => {
         <FlatList
           data={locations}
           showsVerticalScrollIndicator={false}
+          onEndReachedThreshold={0.2}
+          onEndReached={fetchMoreLocations}
+          ListFooterComponent={renderFooter}
           renderItem={({item}) => {
             return (
               <LocationItem
-                key={item.id}
+                key={uuid.v4().toString()}
                 name={item.name}
                 type={item.type}
                 dimension={item.dimension}
